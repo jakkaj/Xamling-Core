66f4ca54f7f7dad146c79001440d9345187366d3
Jordan Knight <jakkaj@xamling.net>
2014-06-17 09:48:42 +1000

    added ViewsModule

---
+++ XamlingCore/XamlingCore.Samples/XamlingCore.Samples.iOS/Glue/Modules/ViewsModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using XamlingCore.Portable.Glue;
+
+namespace XamlingCore.Samples.iOS.Glue.Modules
+{
+    public class ViewsModule : XAutofacModule
+    {
+        public ViewsModule()
+            : base(Assembly.Load("XamlingCore.Samples"), "View", true, false)
+        {
+            
+        }
+    }
+}
