a609ec9103c4a3ae2c3f4f166cbd3b2f403ade2c
Jordan Knight <jakkaj@gmail.com>
2014-11-03 11:56:42 +1100

    Added missing untracked file.
    Thanks Visual Studio GIT. Asshat.

---
+++ XamlingCore/XamlingCore.Portable.Contract/EventArgs/BucketUpdatedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlingCore.Portable.Contract.EventArgs
+{
+    public class BucketUpdatedEventArgs : System.EventArgs
+    {
+        private readonly string _bucket;
+
+        public BucketUpdatedEventArgs(string bucket)
+        {
+            _bucket = bucket;
+        }
+
+        public string Bucket
+        {
+            get { return _bucket; }
+        }
+    }
+}
