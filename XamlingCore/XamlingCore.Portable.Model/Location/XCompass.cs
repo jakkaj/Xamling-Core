25a923a0506130c1fa889116296a99678d2a76b3
Jordan Knight <jakkaj@gmail.com>
2014-11-09 20:09:53 +1100

    Added inteface and entity for compass, no impl yet. Added xswtichcell

---
+++ XamlingCore/XamlingCore.Portable.Model/Location/XCompass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlingCore.Portable.Model.Location
+{
+    public class XCompass
+    {
+        public double Heading { get; set; }
+        public double Accuracy { get; set; }
+        public bool NeedsCalibration { get; set; }
+        public bool IsValid { get; set; }
+        public bool IsSupported { get; set; }
+    }
+}
---
+++ XamlingCore/XamlingCore.Xamarin/Controls/XSwitchCell.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace XamlingCore.XamarinThings.Controls
+{
+    public class XSwitchCell : SwitchCell
+    {
+        public string TextBind
+        {
+            set
+            {
+                SetBinding(TextProperty, new Binding(value, BindingMode.OneWay));
+            }
+        }
+
+        public string OnBind
+        {
+            set
+            {
+                SetBinding(OnProperty, new Binding(value, BindingMode.TwoWay));
+            }
+        }
+    }
+}
