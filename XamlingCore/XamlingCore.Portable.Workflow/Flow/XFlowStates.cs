d6a6f5865877d5a27eb42817341eee7f1f62dcc0
Jordan Knight <jakkaj@gmail.com>
2014-11-26 21:37:51 +1100

    Added missing files

---
+++ XamlingCore/XamlingCore.Portable.Workflow/Flow/XFlowStates.cs
@@ -0,0 +1,14 @@
+﻿namespace XamlingCore.Portable.Workflow.Flow
+{
+    public enum XFlowStates
+    {
+        Success = 0,
+        Fail = 1,
+        InProgress = 2, 
+        WaitingForNetwork = 4,
+        DisconnectedProcessing = 8,
+        WaitingToStart = 16,
+        WaitingForNextStage = 32,
+        WaitingForRetry = 64
+    }
+}
