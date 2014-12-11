f3bab7ffc35614a5c912fb3c94bd2d6c2ee80c9f
Jordan Knight <jakkaj@gmail.com>
2014-12-11 17:25:21 +1100

    Extension to help put padding on forms views

---
+++ XamlingCore/XamlingCore.Xamarin/Content/Extensions/ViewExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlingCore.XamarinThings.Content.Extensions
+{
+    public static class ViewExtensions
+    {
+        public static ContentView Wrap(this View item, Thickness padding = default (Thickness))
+        {
+            if(padding == default (Thickness))
+            {
+                padding = new Thickness(10, 10, 10, 10);
+            }
+
+            var f = new ContentView
+            {
+                Padding = padding,
+                Content = item
+            };
+
+            return f;
+        }
+    }
+}
