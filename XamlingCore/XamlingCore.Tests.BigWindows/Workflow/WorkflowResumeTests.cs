d91effefd13e68cc38484811d421188103d17465
Jordan Knight <jakkaj@gmail.com>
2015-01-19 12:39:38 +1100

    Added a test class for WF Resuming

---
+++ XamlingCore/XamlingCore.Tests.BigWindows/Workflow/WorkflowResumeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using XamlingCore.Tests.BigWindows.Base;
+
+namespace XamlingCore.Tests.BigWindows.Workflow
+{
+    [TestClass]
+    public class WorkflowResumeTests : TestBase
+    {
+        public async Task SetupFlow()
+        {
+            
+        }
+
+        public async Task ContinueFlow()
+        {
+            
+        }
+
+        async void _configureHub()
+        {
+            
+        }
+    }
+}
