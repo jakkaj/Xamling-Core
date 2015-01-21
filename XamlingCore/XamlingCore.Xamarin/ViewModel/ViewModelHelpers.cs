a12e3454392b241988622613776ded022e0da5ba
Jordan Knight <jakkaj@gmail.com>
2015-01-21 16:05:39 +1100

    Added easier view model casting for Xamarin bindable objects

---
+++ XamlingCore/XamlingCore.Xamarin/ViewModel/ViewModelHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamlingCore.Portable.View.ViewModel;
+
+namespace XamlingCore.XamarinThings.ViewModel
+{
+    public static class ViewModelHelpers
+    {
+        public static TViewModel ViewModel<TViewModel>(this BindableObject obj)
+            where TViewModel : XViewModel
+        {
+            return obj.BindingContext as TViewModel;
+        }
+    }
+}
