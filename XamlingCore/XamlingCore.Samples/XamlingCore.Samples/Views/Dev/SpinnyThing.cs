45d5371b0c048d37c43754c25a4d713cd32fd00f
Jordan Knight <jakkaj@gmail.com>
2015-01-23 13:33:34 +1100

    Added a cute little loader

---
+++ XamlingCore/XamlingCore.Samples/XamlingCore.Samples/Views/Dev/SpinnyThing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlingCore.Samples.Views.Dev
+{
+    public class SpinnyThing : ContentView
+    {
+        private bool _stopped;
+       
+        public void Stop()
+        {
+            _stopped = true;
+        }
+
+        public async void Animate()
+        {
+            await this.ScaleTo(.5, 0);
+            _stopped = false;
+            while (!_stopped)
+            {
+                
+                await this.RotateTo(360, 1000, Easing.CubicInOut);
+                await this.RotateTo(0, 0);
+            }
+        }
+    }
+}
