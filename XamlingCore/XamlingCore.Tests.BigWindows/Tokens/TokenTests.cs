354e449ea5889ca1bf6c5d4235abb60cca3e6fed
Jordan Knight <jakkaj@gmail.com>
2015-02-05 15:57:24 +1100

    Added .NET as a server target

---
+++ XamlingCore/XamlingCore.Tests.BigWindows/Tokens/TokenTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using XamlingCore.Windows8.Auth;
+
+namespace XamlingCore.Tests.BigWindows.Tokens
+{
+    [TestClass]
+    public class TokenTests 
+    {
+        [TestMethod]
+        public async Task TokenParseTest()
+        {
+            var result = JsonWebTokenParser.Decode(
+                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJuYW1laWQiOiJGMzgzNDQ0Ny0zRUFBLTRCQ0MtQkZDNS0zMzgxQjREMjQ3MjAiLCJwcm9qZWN0dHJpcG9kL3Rva2VuaWQiOiJjYTc1YzM0Ny0wZTMxLTQxNmUtYTdhYy04ZmIwN2E4YmYzOTQiLCJwcm9qZWN0dHJpcG9kL2F1dGhUb2tlbiI6InRydWUiLCJlbWFpbCI6ImpvcmRhbkBwcm9qZWN0dHJpcG9kLmNvbSIsImlzcyI6IlByb2plY3RUcmlwb2RBdXRoZW50aWNhdG9yIiwiYXVkIjoiaHR0cDovL2FwaS5wcm9qZWN0dHJpcG9kLmNvbSIsImV4cCI6MTQyMzE4MDQxNCwibmJmIjoxNDIzMDkzMTE0fQ.D2uVun-WIhLiNU-KPx029jfdi7dkktZqqZnthTJLdTrCzn2wSI6zFzAHH_qkYocsmYd7MxNiYA5QGvnH_WC5jrD_zPIDxnI69AhroJ8EQrCtoeJN-bfl7OZdk5GqO63lJX3nDljS535lp9XC_EOY0XSZaiRmdYV3kzp8GIg723E-dZnsFoKmfaYOvozTiRmQGbXg7Xz4SrasYpVOTxdSC8nE1FM0Q9tbZvWPs8mIFm13YumfTSFD5-6pfh7fNPp2ZPYz8bYWy9Id6uKGGvG_iL7tvLIK77x_xC3Azo9RcaKtcwrQsDWXGPHSzXIeL3i0KQwr9dunVYFD8FSMc7OeGQ",
+                "PFJTQUtleVZhbHVlPjxNb2R1bHVzPjMwazZvWUk0bEJiMHlCeWN3K29PNW5DYVIvOFoxdU9QNmhiMjVhNGJmVnl3SCtZbUhsZUdtSy82cG1nbDc0SFpNb1pLU1hZelo0Rnl1TmUwTjJjak1jbEhtWXE5TGc1UjVjSHNPQmF5MEp6b29zajhmNFUrdWdEamdzZGsveEY2SFlaVll2MmxZcDV0NVFGVTFOUmIwbk9aZVoySWJzT1dSM1RtaVU4aE4rcThGd3VmdG5sTUN4Ym5wdzZpKzNJa2EwcnhtTjhPK2hjUUFyb0ZqMDFMY3Fhdk9CQ3ZKOUpFTkRCK0cvVXgwR1FwdWI1WFNPTit1OEIvNUV5anNRak8zYVZKQk1FYTRJdUlCcFM0VTRzbmpWZjVMTUMvNzN5bVhHSjFTT1B5VE4rQXZOaTBWK3c2QzNJc0M1Vk1yT0ZJZlQwT0FHNDRHazh1QTEwNnoyem5Ddz09PC9Nb2R1bHVzPjxFeHBvbmVudD5BUUFCPC9FeHBvbmVudD48L1JTQUtleVZhbHVlPg==",
+                true);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ XamlingCore/XamlingCore.Windows8/Auth/JsonWebTokenParser.cs
@@ -0,0 +1,176 @@
+﻿//This is from http://stackoverflow.com/questions/10055158/is-there-a-json-web-token-jwt-example-in-c
+//Cannot find the original author sorry!
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Animation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace XamlingCore.Windows8.Auth
+{
+    public enum JwtHashAlgorithm
+    {
+        RS256,
+        HS384,
+        HS512
+    }
+
+    //class Hasher
+    //{
+    //    private readonly string _key;
+    //    private MacAlgorithmProvider macAlgorithmProvider;
+    //    public Hasher(string hashType, string key)
+    //    {
+    //        _key = key;
+    //        macAlgorithmProvider = MacAlgorithmProvider.OpenAlgorithm(hashType);
+            
+    //    }
+
+
+    //    public string ComputeHash(string value)
+    //    {
+    //        byte[] hash;
+
+    //        IBuffer buffKey = CryptographicBuffer.ConvertStringToBinary(_key, BinaryStringEncoding.Utf8);
+    //        IBuffer buffMsg = CryptographicBuffer.ConvertStringToBinary(value, BinaryStringEncoding.Utf8);
+
+    //        CryptographicKey hmacKey = macAlgorithmProvider.CreateKey(buffKey);
+    //        // Sign the key and message together.
+    //        IBuffer buffHMAC = CryptographicEngine.Sign(hmacKey, buffMsg);
+            
+    //        CryptographicBuffer.CopyToByteArray(buffHMAC, out hash);
+    //        var shaStr = string.Empty;
+
+    //        for (var i = 0; i < hash.Length; i++)
+    //        {
+    //            shaStr += hash[i].ToString("X2");
+    //        }
+
+    //        return shaStr;
+    //    }
+    //}
+
+    public class JsonWebTokenParser
+    {
+        //private static Dictionary<JwtHashAlgorithm, Func<string, string, string>> HashAlgorithms;
+
+        static JsonWebTokenParser()
+        {
+            //HashAlgorithms = new Dictionary<JwtHashAlgorithm, Func<string, string, string>>
+            //{
+            //    { JwtHashAlgorithm.RS256, (key, value) =>
+            //    {
+            //        var sha = new Hasher("HMAC_SHA256", key);  return sha.ComputeHash(value);  } },
+            //    { JwtHashAlgorithm.HS384, (key, value) =>
+            //    {
+            //        var sha = new Hasher("HMAC_SHA384", key);  return sha.ComputeHash(value);  } },
+            //    { JwtHashAlgorithm.HS512, (key, value) =>
+            //    {
+            //        var sha = new Hasher("HMAC_SHA512", key);  return sha.ComputeHash(value);  } }
+            //};
+        }
+
+        //public static string Encode(object payload, string key, JwtHashAlgorithm algorithm)
+        //{
+        //    return Encode(payload, Encoding.UTF8.GetBytes(key), algorithm);
+        //}
+
+        //public static string Encode(object payload, byte[] keyBytes, JwtHashAlgorithm algorithm)
+        //{
+        //    var segments = new List<string>();
+        //    var header = new { alg = algorithm.ToString(), typ = "JWT" };
+
+        //    byte[] headerBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(header, Formatting.None));
+        //    byte[] payloadBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload, Formatting.None));
+        //    //byte[] payloadBytes = Encoding.UTF8.GetBytes(@"{"iss":"761326798069-r5mljlln1rd4lrbhg75efgigp36m78j5@developer.gserviceaccount.com","scope":"https://www.googleapis.com/auth/prediction","aud":"https://accounts.google.com/o/oauth2/token","exp":1328554385,"iat":1328550785}");
+
+        //    segments.Add(Base64UrlEncode(headerBytes));
+        //    segments.Add(Base64UrlEncode(payloadBytes));
+
+        //    var stringToSign = string.Join(".", segments.ToArray());
+
+        //    var bytesToSign = Encoding.UTF8.GetBytes(stringToSign);
+
+        //    byte[] signature = HashAlgorithms[algorithm](keyBytes, bytesToSign);
+        //    segments.Add(Base64UrlEncode(signature));
+
+        //    return string.Join(".", segments.ToArray());
+        //}
+
+        public static string Decode(string token, string key, bool verify = true)
+        {
+            var parts = token.Split('.');
+            var header = parts[0];
+            var payload = parts[1];
+            byte[] crypto = Base64UrlDecode(parts[2]);
+
+            var b1 = Base64UrlDecode(header);
+            var headerJson = Encoding.UTF8.GetString(b1, 0, b1.Length);
+            var headerData = JObject.Parse(headerJson);
+            var b2 = Base64UrlDecode(payload);
+            var payloadJson = Encoding.UTF8.GetString(b2, 0, b2.Length);
+            var payloadData = JObject.Parse(payloadJson);
+
+            //if (verify)
+            //{
+            //    var bytesToSign = Encoding.UTF8.GetBytes(string.Concat(header, ".", payload));
+            //    var keyBytes = Encoding.UTF8.GetBytes(key);
+            //    var algorithm = (string)headerData["alg"];
+
+            //    var signature = HashAlgorithms[GetHashAlgorithm(algorithm)](key, string.Concat(header, ".", payload));
+            //    var decodedCrypto = Convert.ToBase64String(crypto);
+            //   // var decodedSignature = Convert.ToBase64String(signature);
+
+            //    if (decodedCrypto != signature)
+            //    {
+            //        throw new InvalidOperationException(string.Format("Invalid signature. Expected {0} got {1}", decodedCrypto, signature));
+            //    }
+            //}
+
+            return payloadData.ToString();
+        }
+
+        private static JwtHashAlgorithm GetHashAlgorithm(string algorithm)
+        {
+            switch (algorithm)
+            {
+                case "RS256": return JwtHashAlgorithm.RS256;
+                case "HS384": return JwtHashAlgorithm.HS384;
+                case "HS512": return JwtHashAlgorithm.HS512;
+                default: throw new InvalidOperationException("Algorithm not supported.");
+            }
+        }
+
+        // from JWT spec
+        private static string Base64UrlEncode(byte[] input)
+        {
+            var output = Convert.ToBase64String(input);
+            output = output.Split('=')[0]; // Remove any trailing '='s
+            output = output.Replace('+', '-'); // 62nd char of encoding
+            output = output.Replace('/', '_'); // 63rd char of encoding
+            return output;
+        }
+
+        // from JWT spec
+        private static byte[] Base64UrlDecode(string input)
+        {
+            var output = input;
+            output = output.Replace('-', '+'); // 62nd char of encoding
+            output = output.Replace('_', '/'); // 63rd char of encoding
+            switch (output.Length % 4) // Pad with trailing '='s
+            {
+                case 0: break; // No pad chars in this case
+                case 2: output += "=="; break; // Two pad chars
+                case 3: output += "="; break; // One pad char
+                default: throw new System.Exception("Illegal base64url string!");
+            }
+            var converted = Convert.FromBase64String(output); // Standard base64 decoder
+            return converted;
+        }
+    }
+}
