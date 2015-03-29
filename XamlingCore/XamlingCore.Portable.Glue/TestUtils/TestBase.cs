04d10587ac7c6297e1f7449d45c3c54c95050e55
Jordan Knight <jakkaj@gmail.com>
2015-03-30 10:22:58 +1100

    Added test base to the shared class

---
+++ XamlingCore/XamlingCore.Portable.Glue/TestUtils/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace XamlingCore.Portable.Glue.TestUtils
+{
+    public class TestBase<T> where T : GlueBase, new()
+    {
+        protected IContainer Container;
+        public TestBase()
+        {
+            var glue = new T();
+            glue.Init();
+
+            Container = glue.Container;
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            return Container.Resolve<T>();
+        }
+    }
+}
