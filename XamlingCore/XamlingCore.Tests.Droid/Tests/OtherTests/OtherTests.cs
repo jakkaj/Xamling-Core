dba8db40a3dff633e9a843c61c6b04c8337bcf0b
Thomas Knight <tom@xamling.net>
2015-03-31 10:52:10 +1100

    Added missing files

---
+++ XamlingCore/XamlingCore.Tests.Droid/Tests/OtherTests/OtherTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using XamlingCore.Portable.Contract.Entities;
+using XamlingCore.Portable.Data.Extensions;
+using XamlingCore.Tests.Droid.Base;
+using XamlingCore.Portable.Contract.Downloaders;
+
+namespace XamlingCore.Tests.Droid.Tests.OtherTests
+{
+    [TestFixture]
+    public class OtherTests : TestBase
+    {
+        //[Test]
+        //public async void TestBucketTypesNoMSR()
+        //{
+        //    var HttpClient = Resolve<IHttpTransferrer>();
+        //    var t = await HttpClient.Download("/?tom=true");
+        //}        
+    }
+}
+
