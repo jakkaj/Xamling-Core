abedd80f5fcb76d63d2b6c044dce84633c55951b
Jordan Knight <jakkaj@gmail.com>
2015-06-04 10:43:19 +1000

    Added xseverity level to xcore

---
+++ XamlingCore/XamlingCore.Portable.Model/Other/XSeverityLevel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlingCore.Portable.Model.Other
+{
+    public enum XSeverityLevel
+    {
+        //
+        // Summary:
+        //     Verbose severity level.
+        Verbose = 0,
+        //
+        // Summary:
+        //     Information severity level.
+        Information = 1,
+        //
+        // Summary:
+        //     Warning severity level.
+        Warning = 2,
+        //
+        // Summary:
+        //     Error severity level.
+        Error = 3,
+        //
+        // Summary:
+        //     Critical severity level.
+        Critical = 4
+    }
+}
