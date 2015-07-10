1112c850e296a35d822992db5caa370a5601b48b
Jordan Knight <jakkaj@gmail.com>
2015-07-10 11:55:16 +1000

    Updated to use latest nugets, including xamarin forms 1.4.3.

--- XamlingCore/XamlingCore.Samples.Windows8/XamlingCore.Samples.Windows8.WindowsPhone/MainPage.xaml.cs
+++ XamlingCore/XamlingCore.Samples.Windows8/XamlingCore.Samples.Windows8.WindowsPhone/MainPage.xaml.cs
@@ -17,7 +17,7 @@ namespace XamlingCore.Samples.Windows8
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : WindowsPhonePage
+    public sealed partial class MainPage
     {
         private UserControl _loader;
 
