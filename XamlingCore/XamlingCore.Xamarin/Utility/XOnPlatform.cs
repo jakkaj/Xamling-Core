28744d20934d0e5fae66a24c91f7ef8935cd10d7
Jordan Knight <jakkaj@gmail.com>
2015-07-13 11:16:21 +1000

    Added XOnPlatofrm with Windows universal capability

---
+++ XamlingCore/XamlingCore.Xamarin/Utility/XOnPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlingCore.XamarinThings.Utility
+{
+    public class XOnPlatform<T> : OnPlatform<T>
+    {
+        public T Windows { get; set; }
+
+        public static implicit operator T(XOnPlatform<T> onPlatform)
+        {
+            if (Device.OS == TargetPlatform.Windows)
+            {
+                return onPlatform.Windows;
+            }
+
+            return (OnPlatform<T>)onPlatform;
+        }
+    }
+}
