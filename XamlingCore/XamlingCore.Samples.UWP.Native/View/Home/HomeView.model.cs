918a7400583774106d5a9fba82a5f14656d52b06
Jordan Knight <jakkaj@outlook.com>
2016-03-23 15:10:18 +1100

    Added some sample navigations

--- XamlingCore/XamlingCore.Samples.UWP.Native/View/Home/HomeView.model.cs
+++ XamlingCore/XamlingCore.Samples.UWP.Native/View/Home/HomeView.model.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using XamlingCore.Portable.View;
 using XamlingCore.Portable.View.ViewModel;
+using XamlingCore.Samples.UWP.Native.View.Home.Subs;
 
 namespace XamlingCore.Samples.UWP.Native.View.Home
 {
     public class HomeViewModel : XViewModel
     {
+        public ICommand NextPageCommand { get; set; }
+        public HomeViewModel()
+        {
+            NextPageCommand = new XCommand(_onNextPage);
+        }
+
+        void _onNextPage()
+        {
+            NavigateTo<SecondHomePageViewModel>();
+        }
     }
 }
---
+++ XamlingCore/XamlingCore.Samples.UWP.Native/View/Home/Subs/SecondHomePageView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XamlingCore.UWP.Contract;
+using XamlingCore.UWP.View;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace XamlingCore.Samples.UWP.Native.View.Home.Subs
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondHomePageView : XPage, IXPage<SecondHomePageViewModel>
+    {
+        public SecondHomePageView()
+        {
+            this.InitializeComponent();
+        }
+
+        public override void SetViewModel(object vm)
+        {
+            ViewModel = vm as SecondHomePageViewModel;
+        }
+
+        public SecondHomePageViewModel ViewModel { get; set; }
+    }
+}
