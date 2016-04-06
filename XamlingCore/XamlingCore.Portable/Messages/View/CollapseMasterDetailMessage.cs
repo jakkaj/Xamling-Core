3793a238e9b3549b4a2e2591cca24a2c5e4058fd
Jordan Knight <jakkaj@outlook.com>
2016-04-06 10:25:03 +1000

    Added missing files

---
+++ XamlingCore/XamlingCore.Portable/Messages/View/CollapseMasterDetailMessage.cs
@@ -0,0 +1,13 @@
+﻿using XamlingCore.Portable.Messages.XamlingMessenger;
+
+namespace XamlingCore.Portable.Messages.View
+{
+    public class CollapseMasterDetailMessage : XMessage
+    {
+    }
+
+    public class ShowMasterDetailMessage : XMessage
+    {
+        
+    }
+}
