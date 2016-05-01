e88b490ba692ca1c6f4fc9ae9f793e36fa85d2c2
Jordan Knight <jakkaj@gmail.com>
2016-05-01 19:15:31 +1000

    Added sample for requesting permissions for location on droid.

--- XamlingCore/XamlingCore.Samples.Droid/MainActivity.cs
+++ XamlingCore/XamlingCore.Samples.Droid/MainActivity.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Android;
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
 using Xamarin.Forms.Platform.Android;
 using XamlingCore.Samples.Droid.Glue;
 using XamlingCore.Samples.Views.Root.MasterDetailRoot;
@@ -16,6 +22,14 @@ namespace XamlingCore.Samples.Droid
     {
         int count = 1;
 
+        readonly string[] PermissionsLocation =
+       {
+          Manifest.Permission.AccessCoarseLocation,
+          Manifest.Permission.AccessFineLocation
+       };
+
+        const int RequestLocationId = 0;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -23,6 +37,35 @@ namespace XamlingCore.Samples.Droid
             var xapp = new App();
             xapp.Init<RootMasterDetailViewModel, ProjectGlue>();
             LoadApplication(xapp);
+
+        }
+
+        public async Task AskLocationPermissions()
+        {
+            if ((int) Build.VERSION.SdkInt < 23)
+            {
+                return;
+            }
+
+            const string permission = Manifest.Permission.AccessFineLocation;
+
+            if (ContextCompat.CheckSelfPermission(this, permission) == (int)Permission.Granted)
+            {
+                return;
+            }
+
+            if (ActivityCompat.ShouldShowRequestPermissionRationale(this, permission))
+            {
+                //Explain to the user why we need to read the contacts
+                Snackbar.Make(this.FindViewById(Android.Resource.Id.Content), "Location access is required to show nearby stores.",
+                    Snackbar.LengthIndefinite)
+                    .SetAction("OK", v => RequestPermissions(PermissionsLocation, RequestLocationId))
+                    .Show();
+
+                return;
+            }
+
+            RequestPermissions(PermissionsLocation, RequestLocationId);
         }
     }
 }
